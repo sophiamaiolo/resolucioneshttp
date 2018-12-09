d2cfae39477811654df9e3d1d8ff9f6bf7ef2084
xtebanuy <esteban.soba@gmail.com>
2018-12-09 17:47:08 -0300

    cambios en ruteo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListadoRepartidosComponent } from './listado-repartidos/listado-repartidos.component';
+import { YasqeEditorComponent } from './yasqe-editor/yasqe-editor.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: "", component: ListadoRepartidosComponent}, {path: "query", component: YasqeEditorComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
